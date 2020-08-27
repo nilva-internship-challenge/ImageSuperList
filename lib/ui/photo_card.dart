345e76887a127eb864261b44a9705d8da7c41db4
ftadev <akhlaghi.fatemeh@gmail.com>
2020-08-27 11:33:36 +0430

    Change UI

--- lib/ui/photo_card.dart
+++ lib/ui/photo_card.dart
@@ -2,30 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:nilva_image_super_list/repository/model/photo_model.dart';
 
-class PhotoCard extends StatelessWidget {
+class PhotoCard extends StatefulWidget {
   final PhotoModel photoModel;
 
   const PhotoCard({Key key, this.photoModel}) : super(key: key);
 
   @override
+  _PhotoCardState createState() => _PhotoCardState();
+}
+
+class _PhotoCardState extends State<PhotoCard> {
+  bool isLiked = false;
+
+  @override
   Widget build(BuildContext context) {
-    decreaseSize(photoModel.downloadUrl);
+    decreaseSize(widget.photoModel.downloadUrl);
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(5.0),
       ),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(5.0),
-            child: Image.network(
-              decreaseSize(photoModel.downloadUrl),
-              fit: BoxFit.fill,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.3,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: Image.network(
+                decreaseSize(widget.photoModel.downloadUrl),
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Opacity(
+                opacity: 0.3,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      widget.photoModel.author,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      icon: Icon(
+                        isLiked ? Icons.favorite : Icons.favorite_border,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isLiked = !isLiked;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      width: 8,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
